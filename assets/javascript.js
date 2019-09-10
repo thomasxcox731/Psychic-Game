33ddd2e5d30ac1b99c9397fc624793e6c931c997
Tom Cox <thomasxcox731@gmail.com>
2019-09-09 22:08:12 -0500

    It is alive! It is styled as well.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,14 +1,14 @@
 
-//define variables, arrays//
+//Define variables, arrays//
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
-var guessLeft = 9;
-// var guessFar = [];
+var guessesLeft = 9;
 var userGuesses = "";
 
-//onkeyup functions//
+//The onkeyup functions//
 document.onkeyup = function (event) {
+    //User hits the key, creates event//
     userGuess = event.key.toLowerCase();
     console.log(userGuess)
     //computer random generator//
@@ -17,20 +17,20 @@ document.onkeyup = function (event) {
     // if the userGuess is equal to the computerGuess, the user gets a point, the score is updated, and the game is reset//
     if (userGuess === computerGuess) {
         wins++;
-        guessLeft = 9;
+        guessesLeft = 9;
     }
-    //if the userGuess doesn't equal the computerGuess, the guessesLeft decreases by one, the guessesLeft score is updates, the wrong answers are listed//
+    //if the userGuess doesn't equal the computerGuess, the guessesLeft decreases by one, the wrong answers are listed//
     else {
-        guessLeft--;
+        guessesLeft--;
         userGuesses = userGuesses + " " + userGuess;
     }
-    //if the guesses left run out, need to add a loss, update the score and reset game//
-    if (guessLeft <= 0) {
+    //if the guesses left run out, need to add a loss and reset game//
+    if (guessesLeft <= 0) {
         losses++;
-        guessLeft = 9;
+        guessesLeft = 9;
         userGuesses = "";
     }
-    //DEFINITIONS//
+    //Updating the Scoreboard//
     var winsText = document.getElementById("wins");
         winsText.textContent = "Wins: " + wins;
     var lossesText = document.getElementById("losses");
